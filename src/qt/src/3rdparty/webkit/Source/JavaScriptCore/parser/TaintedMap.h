2cd1cb39772c93f996f6ce9e5ca59abc7efac4bb
Nera Liu <neraliu@yahoo-inc.com>
2014-04-10 06:06:18 +0000

    add the new class TaintedMap for storing the UString tainted signal.

---
+++ src/qt/src/3rdparty/webkit/Source/JavaScriptCore/parser/TaintedMap.h
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2014 Yahoo! Inc. All Rights Reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY YAHOO! INC. ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL YAHOO! INC. OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+#ifndef TaintedMap_h
+#define TaintedMap_h
+
+#include <string>
+#include <map>
+
+using namespace std;
+
+namespace JSC {
+
+	class TaintedMap {
+                public:
+                        static TaintedMap* getInstance();
+			void add(string l, unsigned int t);
+			void update(string l, unsigned int t);
+			unsigned int get(string l);
+			void remove(string l);
+			void clear();
+                private:
+                        TaintedMap();
+                        ~TaintedMap();
+                        static TaintedMap* m_tainted_map;
+			map<string, unsigned int> m_map;
+	};
+
+} // namespace JSC
+
+#endif // TaintedMap_h
